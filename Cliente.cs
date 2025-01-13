04ef61cc7f8720f8b38e806fcb8018540f1a2dd9
William Matsunaga <williamsilvamatsunaga@outlook.com>
2025-01-13 17:18:38 -0400

    Primeiro commit

---
+++ Cliente.cs
@@ -0,0 +1,37 @@
+﻿public class Cliente
+{
+    private int id_cliente;
+    private string nome;
+    private string cpf;
+    private string email;
+    private string telefone;
+    private string endereco;
+
+    public void SetTratamentoCliente(string nome)
+    {
+        try
+        {
+            if (nome.Length >= 50)
+            {
+                throw new Exception("Deve-se conter no máximo 50 caracteres.");
+            }
+            foreach (char c in nome)
+            {
+                if (!char.IsLetter(c) && c != ' ')
+                {
+                    throw new Exception("O nome deve conter apenas letras e números.");
+                }
+            }
+
+            this.nome = nome;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    public string GetNome()
+    {
+        return $"{this.nome}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Cliente c = new Cliente();
+
+        int escolha = 0;
+
+        do
+        {
+            Console.WriteLine("Digite 1 p/ cadastrar cliente\n" +
+                "Digite 2 p/ listar todos os clientes\n" +
+                "Digite 3 p/ remover determinado cliente\n" +
+                "Digite 4 p/ atualizar dados de um cliente\n" +
+                "Digite 0 p/ sair do programa:");
+            escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    {
+                        try
+                        {
+                            Console.Write("Digite o nome do cliente: ");
+                            string nome = Console.ReadLine();
+
+                            c.SetTratamentoCliente(nome);
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine($"Erro ao cadastrar cliente!+ {ex.Message}");
+                        }
+
+                
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine($"{c.GetNome()}");
+
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+            }
+
+            Console.Write("Digite 1 /p acessar o menu ou 0 p/ fechar o programa: ");
+            escolha = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+        } while (escolha != 0);
+    }
+}
